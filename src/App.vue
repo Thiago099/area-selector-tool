84bb852573d75cdb0fd593d048ddec3970fdc7df
Thiago099 <tecnologiasthiago@gmail.com>
2022-05-25 13:38:05 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import selectorTool from '@/components/selector-tool.vue'
+const selection = ref()
+</script>
+
+<template>
+  <div>
+      <selector-tool @select="selection = $event"/>
+      {{selection}}
+  </div>
+</template>
+
+<style>
+#app{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
---
+++ src/components/selector-tool.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { onMounted, ref,defineEmits } from 'vue'
+const emit = defineEmits(['select'])
+const rectangle = ref()
+const x = ref(0)
+const y = ref(0)
+const left = ref(0)
+const top = ref(0)
+const width = ref(0)
+const height = ref(0)
+const drag = ref(false)
+function mousemove(ev:MouseEvent)
+{
+    const {clientX, clientY} = ev
+    if(clientX < x.value)
+    {
+        left.value = clientX
+        width.value = x.value - clientX
+    }
+    else
+    {
+        width.value = clientX - x.value
+    }
+    if(clientY < y.value)
+    {
+        top.value = clientY
+        height.value = y.value - clientY
+    }
+    else
+    {
+        height.value = clientY - y.value
+    }
+}
+document.onmousedown = (ev:MouseEvent)=>{
+    ev.preventDefault()
+    const {clientX, clientY} = ev
+    x.value = clientX
+    y.value = clientY
+    left.value = clientX
+    top.value = clientY
+    width.value = 0
+    height.value = 0
+    document.onmousemove = mousemove
+    drag.value = true
+}
+document.onmouseup = (ev:MouseEvent)=>{
+    const {clientX, clientY} = ev
+    const left = clientX < x.value ? clientX : x.value
+    const top = clientY < y.value ? clientY : y.value
+    const right = clientX > x.value ? clientX : x.value
+    const bottom = clientY > y.value ? clientY : y.value
+    emit('select',{
+        left: left,
+        top: top,
+        right: right,
+        bottom: bottom
+    })
+    document.onmousemove = null
+    drag.value = false
+}
+
+</script>
+
+<template>
+<div ref="rectangle" class="rectangle" :style="`left:${left}px;top:${top}px;width:${width}px;height:${height}px;display:${drag?'visible':'none  '}`">
+</div>
+</template>
+
+<style scoped>
+.rectangle{
+    position: fixed;
+    z-index: 9999;
+    border : 2px dashed rgb(4, 70, 253);
+    background-color: rgba(4, 70, 253, 0.1);
+}
+</style>
